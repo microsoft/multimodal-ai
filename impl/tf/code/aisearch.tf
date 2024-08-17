5a24f3b9442a5940531b0416e355dfb788f7229f
uday31in <14359777+uday31in@users.noreply.github.com>
2024-08-18 00:14:06 +0100

    create index

--- impl/tf/code/aisearch.tf
+++ impl/tf/code/aisearch.tf
@@ -1,34 +1,34 @@
-module "ai_search"{
-    source = "./modules/aisearch"
+module "ai_search" {
+  source = "./modules/aisearch"
 
-    location = local.location
-    resource_group_name = azurerm_resource_group.mmai.name
-    tags = var.tags
+  location            = local.location
+  resource_group_name = azurerm_resource_group.mmai.name
+  tags                = var.tags
 
-    search_service_name = local.ai_search_name
-    search_service_sku = local.ai_search_sku
-    search_service_partition_count = 1
-    search_service_replica_count = 1
-    customer_managed_key = null
-    log_analytics_workspace_id = module.azure_log_analytics.log_analytics_id
-    user_assigned_identity_id = module.user_assigned_identity.user_assigned_identity_id
-    subnet_id = null
+  search_service_name            = local.ai_search_name
+  search_service_sku             = local.ai_search_sku
+  search_service_partition_count = 1
+  search_service_replica_count   = 1
+  customer_managed_key           = null
+  log_analytics_workspace_id     = module.azure_log_analytics.log_analytics_id
+  user_assigned_identity_id      = module.user_assigned_identity.user_assigned_identity_id
+  subnet_id                      = null
 
-    # cognitive_service_kind = "search"
-    # cognitive_service_name = local.ai_search_name
-    # cognitive_service_sku = local.ai_search_sku
+  # cognitive_service_kind = "search"
+  # cognitive_service_name = local.ai_search_name
+  # cognitive_service_sku = local.ai_search_sku
 
-    # user_assigned_identity_id = azurerm_user_assigned_identity.user_assigned_identity.id
-    # subnet_id = null
-    # customer_managed_key = null
-    # log_analytics_workspace_id = module.azure_log_analytics.log_analytics_id
+  # user_assigned_identity_id = azurerm_user_assigned_identity.user_assigned_identity.id
+  # subnet_id = null
+  # customer_managed_key = null
+  # log_analytics_workspace_id = module.azure_log_analytics.log_analytics_id
 }
 
 
 locals {
   mmai_text_datasource_json = {
-    name : "mmai-text-files",
-    description : "mmai-text-files",
+    name : "mmai-text-files-dataource",
+    description : "mmai-text-files-dataource",
     type : "azureblob",
     credentials : {
       connectionString : module.storage_account.storage_account_primary_connection_string
@@ -48,20 +48,103 @@ resource "restapi_object" "ai_search_datasource_mmai_text" {
   path         = "/datasources"
   query_string = "api-version=2023-10-01-Preview"
   data         = jsonencode(local.mmai_text_datasource_json)
-  id_attribute = "name" # The ID field on the response
+  id_attribute =  "name"
   depends_on = [
     module.storage_account,
     module.ai_search
   ]
 }
 
-# resource "restapi_object" "ai_search_datasource_mmai_text" {
-#   path         = "/datasources"
-#   query_string = "api-version=2023-10-01-Preview"
-#   data         = jsonencode(local.mmai_text_datasource_json)
-#   id_attribute = "name" # The ID field on the response
-#   depends_on = [
-#     module.storage_account,
-#     module.ai_search
-#   ]
-# }
+locals {
+  mmai_text_index_json = {
+    name = "mmai-index-text-files-dataource",
+    fields = [
+      {
+        name       = "id"
+        type       = "Edm.String"
+        searchable = false
+        filterable = false
+        sortable   = false
+        key        = true
+        facetable  = false
+      },
+      {
+        name       = "metadata_storage_last_modified"
+        type       = "Edm.DateTimeOffset"
+        searchable = false
+        filterable = true
+        sortable   = false
+        facetable  = false
+      },
+      {
+        name       = "title"
+        type       = "Edm.String"
+        searchable = true
+        filterable = true
+        sortable   = true
+        facetable  = false
+      },
+      {
+        name       = "metadata_storage_name"
+        type       = "Edm.String"
+        searchable = true
+        filterable = true
+        sortable   = true
+        facetable  = false
+      },
+      {
+        name       = "metadata_storage_path"
+        type       = "Edm.String"
+        searchable = true
+        filterable = true
+        sortable   = true
+        facetable  = false
+      },
+      {
+        name       = "metadata_storage_content_md5"
+        type       = "Edm.String"
+        filterable = true
+        sortable   = true
+        facetable  = false
+      },
+      {
+        name       = "content"
+        type       = "Edm.String"
+        searchable = true
+        filterable = false
+        sortable   = false
+        facetable  = false
+      },
+    ],
+    semantic = {
+      configurations = [
+        {
+          name : "mmai-index-text-files-dataource",
+          prioritizedFields = {
+            titleField = {
+              fieldName = "title"
+            },
+            prioritizedContentFields = [
+              {
+                fieldName = "content"
+              }
+            ],
+            prioritizedKeywordsFields = []
+          }
+        }
+      ]
+    },
+  }
+}
+
+# https://learn.microsoft.com/en-us/rest/api/searchservice/create-index
+resource "restapi_object" "ai_search_index_mmai_text" {
+  path         = "/indexes"
+  query_string = "api-version=2023-10-01-Preview"
+  data         = jsonencode(local.mmai_text_index_json)
+  id_attribute = "name"
+  depends_on = [
+    module.ai_search,
+    restapi_object.ai_search_datasource_mmai_text
+  ]
+}
