6668b1b7bdcc2d881e5cd70bc6cf91489effe45a
uday31in <14359777+uday31in@users.noreply.github.com>
2024-08-25 20:36:14 +0100

    adding smeantic search

--- deployment/terraform/code/modules/aisearch/aisearch.tf
+++ deployment/terraform/code/modules/aisearch/aisearch.tf
@@ -24,6 +24,7 @@ resource "azurerm_search_service" "search_service" {
   local_authentication_enabled             = true
   customer_managed_key_enforcement_enabled = false
   authentication_failure_mode              = "http403"
+  semantic_search_sku                      = "standard"
 }
 
 resource "azurerm_role_assignment" "identity_access_to_search" {
