3e4d0c2565704a9cd2b79787b65fa9ff9e0acba1
onderyildirim <ondery@microsoft.com>
2024-11-13 20:31:29 -0500

    add local definition

--- deployment/terraform/locals.tf
+++ deployment/terraform/locals.tf
@@ -6,6 +6,7 @@ locals {
 # Generate a unique resource token
 locals {
   resourceToken = lower(join("", [random_id.random.hex]))
+  is_windows     = length(regexall("^[a-z]:", lower(abspath(path.root)))) > 0
 }
 
 # Define tags for resources
@@ -22,3 +23,4 @@ resource "random_id" "random" {
   }
   byte_length = 4
 }
+
