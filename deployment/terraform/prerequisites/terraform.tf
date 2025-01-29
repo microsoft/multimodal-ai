155769fca3db2e73371f2d488b37fdb2941bbe0f
Vitaliy Slepakov <v.slepakov@gmail.com>
2025-01-29 10:58:58 +0100

    use oidc

--- deployment/terraform/prerequisites/terraform.tf
+++ deployment/terraform/prerequisites/terraform.tf
@@ -30,7 +30,6 @@ terraform {
     storage_account_name = "<provided-via-config>"
     container_name       = "<provided-via-config>"
     key                  = "<provided-via-config>"
-    use_azuread_auth     = true
   }
 }
 
