deac35a9e1277a1d424993b4ec7dbf70ba3c5f82
ksaye <ksaye@saye.org>
2025-01-29 16:53:42 -0600

    Update outputs.tf
    Had to add the sensitive = true else the 'terraform apply -var-file .\terraform.tfvars -var-file .\prereqs.tfvars' failed with a warning.

--- deployment/terraform/infra/outputs.tf
+++ deployment/terraform/infra/outputs.tf
@@ -42,5 +42,6 @@ locals {
 }
 
 output "cleanup_command" {
+  sensitive = true
   value = "${local.rg_delete_command}${local.adapp_delete_command_function}${local.adapp_delete_command_webapp_client}${local.adapp_delete_command_webapp_server}"
 }
