99186a97a30bf33fe347f1aba867e6188bd2f55f
Vitaliy Slepakov <v.slepakov@gmail.com>
2025-02-10 09:20:38 +0100

    fix

--- deployment/terraform/runner/containerapps.tf
+++ deployment/terraform/runner/containerapps.tf
@@ -52,7 +52,7 @@ resource "azapi_resource" "container_apps_job" {
 
   depends_on = [
     azurerm_role_assignment.uai_role_assignment_key_vault_secrets_user,
-    azurerm_azurerm_role_assignment.current_role_assignment_key_vault_secrets_officer
+    azurerm_role_assignment.current_role_assignment_key_vault_secrets_officer
   ]
 
   body = {
