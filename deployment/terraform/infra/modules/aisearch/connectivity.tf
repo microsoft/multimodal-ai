957e16c63430199c8d4e4c006a7bf5e954c6986b
Kevin Saye <ksaye@saye.org>
2025-02-10 13:45:54 -0600

    Adding Shared private access from Azure Search to AOIA and Blob Storage

--- deployment/terraform/infra/modules/aisearch/connectivity.tf
+++ deployment/terraform/infra/modules/aisearch/connectivity.tf
@@ -40,6 +40,43 @@ resource "azurerm_private_endpoint" "private_endpoint_search_service" {
   ]
 }
 
+resource "azurerm_search_shared_private_link_service" "shared_private_link_search_service_aoai" {
+  name                = "${var.search_service_name}-spa-aoai"
+  search_service_id   = azurerm_search_service.search_service.id
+  subresource_name    = "openai_account"
+  target_resource_id  = var.openai_account_id  
+  request_message     = "Auto approved"
+  depends_on = [ null_resource.ai_search_disable_public_network_access ]
+}
+
+resource "azurerm_search_shared_private_link_service" "shared_private_link_search_service_storage" {
+  name                = "${var.search_service_name}-spa-strg"
+  search_service_id   = azurerm_search_service.search_service.id
+  subresource_name    = "blob"
+  target_resource_id  = var.storage_account_id
+  request_message     = "Auto approved"
+  depends_on = [ null_resource.ai_search_disable_public_network_access ]
+}
+
+resource "null_resource" "ai_search_approve_shared_private_link" {
+  provisioner "local-exec" {
+    interpreter = local.is_windows ? ["PowerShell", "-Command"] : []
+    command     = <<-EOT
+      $aoai_id = $(az network private-endpoint-connection list --id ${var.openai_account_id} --query "[?contains(properties.privateEndpoint.id, 'vnet')].id" -o json) | ConvertFrom-Json
+      $strg_id = $(az network private-endpoint-connection list --id ${var.storage_account_id} --query "[?contains(properties.privateEndpoint.id, 'vnet')].id" -o json) | ConvertFrom-Json
+      az network private-endpoint-connection approve --id $aoai_id --description "Auto-Approved"
+      az network private-endpoint-connection approve --id $strg_id --description "Auto-Approved"
+    EOT
+  }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+  depends_on = [ 
+    azurerm_search_shared_private_link_service.shared_private_link_search_service_aoai,
+    azurerm_search_shared_private_link_service.shared_private_link_search_service_storage 
+    ]
+}
+
 resource "time_sleep" "sleep_connectivity" {
   create_duration = "${var.connectivity_delay_in_seconds}s"
 
