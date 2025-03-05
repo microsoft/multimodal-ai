6d01e0a7984fde8ed6fb3cc2bbf3c7ea8587334b
ksaye <ksaye@saye.org>
2025-03-05 15:00:11 -0600

    resolving dependencies

--- deployment/terraform/infra/modules/aisearch/connectivity.tf
+++ deployment/terraform/infra/modules/aisearch/connectivity.tf
@@ -17,12 +17,18 @@ resource "null_resource" "ai_search_disable_public_network_access" {
     azapi_update_resource.open_ai_azure_search_private_endpoint_approver,
     azapi_update_resource.computer_vision_azure_search_private_endpoint_approver,
     azapi_update_resource.ai_multi-service_azure_search_private_endpoint_approver,
-    //    azapi_update_resource.form_recognition_azure_search_private_endpoint_approver,
+//    azapi_update_resource.form_recognition_azure_search_private_endpoint_approver,
     azapi_update_resource.function_azure_search_private_endpoint_approver
   ]
 }
 
 resource "azurerm_private_endpoint" "private_endpoint_search_service" {
+  depends_on         = [
+    null_resource.create_datasource,
+    null_resource.create_index,
+    null_resource.create_skillset,
+    null_resource.create_indexer,
+    ] # race / conflict conditions if too many updates are happening to AI Search
   name                = "${var.search_service_name}-pe"
   location            = var.vnet_location
   resource_group_name = var.resource_group_name
@@ -43,12 +49,7 @@ resource "azurerm_private_endpoint" "private_endpoint_search_service" {
 }
 
 resource "azurerm_search_shared_private_link_service" "shared_private_link_search_service_aoai" {
-  depends_on = [
-    null_resource.create_datasource,
-    null_resource.create_index,
-    null_resource.create_skillset,
-    null_resource.create_indexer,
-  ] # race / conflict conditions if too many updates are happening to AI Search
+  depends_on         = [azurerm_private_endpoint.private_endpoint_search_service]
   name               = "${var.search_service_name}-spa-aoai"
   search_service_id  = azurerm_search_service.search_service.id
   subresource_name   = "openai_account"
@@ -86,7 +87,7 @@ resource "azurerm_search_shared_private_link_service" "shared_private_link_ai_vi
 }*/
 
 resource "azurerm_search_shared_private_link_service" "shared_private_link_ai_multi-service" {
-  depends_on = [azurerm_search_shared_private_link_service.shared_private_link_ai_vision] # can only add one at a time
+  depends_on         = [azurerm_search_shared_private_link_service.shared_private_link_ai_vision] # can only add one at a time
   //depends_on         = [azurerm_search_shared_private_link_service.shared_private_link_form_recognition] # can only add one at a time
   name               = "${var.search_service_name}-spa-cog-multi"
   search_service_id  = azurerm_search_service.search_service.id
