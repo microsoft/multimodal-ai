a79bc642500eb66437aece9f812a3d631a7f8633
ksaye <ksaye@saye.org>
2025-03-05 09:57:20 -0600

    address race conditions on AI Search

--- deployment/terraform/infra/modules/aisearch/connectivity.tf
+++ deployment/terraform/infra/modules/aisearch/connectivity.tf
@@ -17,7 +17,7 @@ resource "null_resource" "ai_search_disable_public_network_access" {
     azapi_update_resource.open_ai_azure_search_private_endpoint_approver,
     azapi_update_resource.computer_vision_azure_search_private_endpoint_approver,
     azapi_update_resource.ai_multi-service_azure_search_private_endpoint_approver,
-    azapi_update_resource.form_recognition_azure_search_private_endpoint_approver,
+//    azapi_update_resource.form_recognition_azure_search_private_endpoint_approver,
     azapi_update_resource.function_azure_search_private_endpoint_approver
   ]
 }
@@ -43,6 +43,12 @@ resource "azurerm_private_endpoint" "private_endpoint_search_service" {
 }
 
 resource "azurerm_search_shared_private_link_service" "shared_private_link_search_service_aoai" {
+  depends_on         = [
+    null_resource.create_datasource,
+    null_resource.create_index,
+    null_resource.create_skillset,
+    null_resource.create_indexer,
+    ] # race / conflict conditions if too many updates are happening to AI Search
   name               = "${var.search_service_name}-spa-aoai"
   search_service_id  = azurerm_search_service.search_service.id
   subresource_name   = "openai_account"
@@ -70,14 +76,14 @@ resource "azurerm_search_shared_private_link_service" "shared_private_link_ai_vi
   request_message    = "Auto-Approved"
 }
 
-resource "azurerm_search_shared_private_link_service" "shared_private_link_form_recognition" {
+/*resource "azurerm_search_shared_private_link_service" "shared_private_link_form_recognition" {
   depends_on         = [azurerm_search_shared_private_link_service.shared_private_link_ai_vision] # can only add one at a time
   name               = "${var.search_service_name}-spa-cog-fr"
   search_service_id  = azurerm_search_service.search_service.id
   subresource_name   = "cognitiveservices_account"
   target_resource_id = var.form_recognizer_id
   request_message    = "Auto-Approved"
-}
+}*/
 
 resource "azurerm_search_shared_private_link_service" "shared_private_link_ai_multi-service" {
   depends_on         = [azurerm_search_shared_private_link_service.shared_private_link_form_recognition] # can only add one at a time
@@ -129,7 +135,7 @@ resource "azapi_update_resource" "computer_vision_azure_search_private_endpoint_
   }
 }
 
-resource "azapi_update_resource" "form_recognition_azure_search_private_endpoint_approver" {
+/*resource "azapi_update_resource" "form_recognition_azure_search_private_endpoint_approver" {
   depends_on = [
     azurerm_search_shared_private_link_service.shared_private_link_form_recognition
   ]
@@ -143,7 +149,7 @@ resource "azapi_update_resource" "form_recognition_azure_search_private_endpoint
       }
     }
   }
-}
+}*/
 
 resource "azapi_update_resource" "ai_multi-service_azure_search_private_endpoint_approver" {
   depends_on = [
