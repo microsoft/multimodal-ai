ec1c890de2f6b0282c9be5692133b62687928f69
ksaye <ksaye@saye.org>
2025-03-11 09:37:49 -0500

    Update connectivity.tf (#203)

--- deployment/terraform/infra/modules/function/connectivity.tf
+++ deployment/terraform/infra/modules/function/connectivity.tf
@@ -6,7 +6,7 @@ resource "azurerm_private_endpoint" "function_private_endpoint" {
   resource_group_name = var.resource_group_name
   subnet_id           = var.private_subnet_id
 
-  custom_network_interface_name = "function_private_endpoint_nic"
+  custom_network_interface_name = "${var.function_name}-nic"
   private_dns_zone_group {
     name                 = "${var.function_name}-arecord"
     private_dns_zone_ids = [var.private_dns_zone_id_sites]
